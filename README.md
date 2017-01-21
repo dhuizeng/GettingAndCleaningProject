0f40f04367c0242c4110e39695cf66d099ebf2b0
dhuizeng <dhuizeng@gmail.com>
2017-01-20 21:02:16 -0500

    Added comments to code and created a README

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# GettingAndCleaningProject
+# Getting and Cleaning Data Project
+
+This R code takes the training and test data sets from the UCI Machine Learning Repository and
+merges them into a single data set, retaining only the average values of the mean and standard deviation
+of the measured quantities for set of test subjects and activities.
+
+## Generating the Tidy data set
+
+To generate the file "Tidydata.txt", first place the file "run_analysis.R" into a directory above the "UCI HAR Dataset" folder. Make sure you have the package "dplyr" installed, as the script utilizes this package. Run the Rscript "run_analysis.R", and you should find the tidy data set!
+
+## Loading the data set
+
+To load the data set into R, simply place "Tidydata.txt" into your working directory and run the command "load.table("Tidydata.txt")". 
+
--- run_analysis.R
+++ run_analysis.R
@@ -1,32 +1,44 @@
+## This library is required for arrange()
+
 library(dplyr)
-## Get Test data, activity labels and subject labels
 
-# testdata <- read.table("UCI HAR Dataset/test/X_test.txt",sep="",header = FALSE)
-# testsublabels <- scan("UCI HAR Dataset/test/subject_test.txt")
-# testactlabels <- scan("UCI HAR Dataset/test/y_test.txt")
-# 
-# traindata <- read.table("UCI HAR Dataset/train/X_train.txt",sep="",header = FALSE)
-# trainsublabels <- scan("UCI HAR Dataset/train/subject_train.txt")
-# trainactlabels <- scan("UCI HAR Dataset/train/y_train.txt")
-# 
-# features <- readLines("UCI HAR Dataset/features.txt")
+## Get Test data, activity labels, subject labels, and column names. Make sure you are
+## in a directory directly above the "UCR HAR Dataset" folder.
+
+testdata <- read.table("UCI HAR Dataset/test/X_test.txt",sep="",header = FALSE)
+testsublabels <- scan("UCI HAR Dataset/test/subject_test.txt")
+testactlabels <- scan("UCI HAR Dataset/test/y_test.txt")
+
+traindata <- read.table("UCI HAR Dataset/train/X_train.txt",sep="",header = FALSE)
+trainsublabels <- scan("UCI HAR Dataset/train/subject_train.txt")
+trainactlabels <- scan("UCI HAR Dataset/train/y_train.txt")
+
+features <- readLines("UCI HAR Dataset/features.txt")
+
+## This gets all of the column numbers and names which include "mean()" or "std()" in their names.
+
 colnums <- which(grepl("mean\\(\\)|std\\(\\)", features))
-
 secondelement <- function(x){x[2]}
 colname <- tolower(c("Activity","Subject ID",sapply(strsplit(features[colnums]," "),secondelement)))
 
-
+## Here we get the subject labels for the training and test data sets. These are looped over
+## in the folliwing bit of code.
 
 testsubs <- as.numeric(levels(factor(testsublabels)))
 trainsubs <- as.numeric(levels(factor(trainsublabels)))
 
+## Here we first create an empty data frame and then fill it with the two sets of nested
+## loops below. The first nested loop runs over the subjects in the test set and subsets
+## out the appropriate rows and columns that correspond to a given activity. The means of
+## each column are then taken and row-bound to the data frame. The task is then repeated
+## for the training data set.
 
 DF <- data.frame()
 for(subjid in testsubs){
         for(actid in 1:6){
-                sub2 <- which(testsublabels == subjid & testactlabels == actid)
-                data2 <-c(actid, subjid, colMeans(testdata[sub2,colnums]))
-                DF <- rbind(DF,data2)
+                sub <- which(testsublabels == subjid & testactlabels == actid)
+                data <-c(actid, subjid, colMeans(testdata[sub,colnums]))
+                DF <- rbind(DF,data)
         }
 }
 
@@ -38,11 +50,17 @@ for(subjid in trainsubs){
         }
 }
 
+## The data frame DF now contains 68 variables and 180 observations (30 subjects * 6 activities)
+## but it needs to be made a litte nicer. First it is arranged by subject id (unnamed as X2 right now).
+## The data frame is then passed the names of each column and given labels to the activity factors.
 
 DF <- arrange(DF,X2)
 colnames(DF) <- colname
 DF$activity <-factor(DF$activity,levels = 1:6,labels = c("walking","walking up stairs",
                                                         "walking down stairs",
                                                         "sitting", "standing", "laying"))
+
+## Finally, write the data out to "Tidydata.txt".
+
 write.table(DF,"Tidydata.txt",row.name =FALSE)
 
